91e14f8e21b61a9552a14bf283bed4c9581d8705
Lanzer <lanzer22@yahoo.com>
2023-02-16 16:49:12 -0800

    [caption] got rid of panda, added param for ext and output

--- captionreader.py
+++ captionreader.py
@@ -1,6 +1,6 @@
 import argparse
 import glob
-import pandas as pd
+import array as arr
   
 def main(args):
     # path = "/home/lanzer/training_data/images/50_pixelle"
@@ -8,25 +8,27 @@ def main(args):
         print(f'Error - no source directory defined.  use --src_dir=""')
         return
     path = args.src_dir
-    files = glob.glob(path + "/*.caption")
+    ext = args.ext
+    files = glob.glob(path + "/*." + ext)
   
-    # defining an empty list to store 
     # content
-    data_frame = pd.DataFrame()
     content = []
   
-    # checking all the csv files in the 
-    # specified path
     for filename in files:
-    
-        # reading content of csv file
-        # content.append(filename)
-        df = pd.read_csv(filename, index_col=None)
-        content.append(df)
+        txtfile = open(filename, "r")
+        file_content = txtfile.read()
+        content.append(file_content)
+        txtfile.close()
   
     # converting content to data frame
-    data_frame = pd.concat(content)
-    print(data_frame)
+    text_output = ''.join(content)
+    print(text_output)
+    
+    if (args.output_file != None):
+        txtfile = open(args.output_file, "w")
+        txtfile.writelines(text_output)
+        txtfile.close()
+    
 
 if __name__ == '__main__':
 
@@ -34,6 +36,13 @@ if __name__ == '__main__':
     parser.add_argument(
         '--src_dir', type=str,
         help='Directory to load images')
+    parser.add_argument(
+        '--output_file', type=str, default=None,
+        help='save output to filename')
+    parser.add_argument(
+        '--ext', type=str, default='caption',
+        help='extension name. default=caption')
+    
     args = parser.parse_args()
 
 
